ebcdb88eed8926e4c2677255772b26875615be41
青涩后妈 <evtjk@hotmail.com>
2024-11-11 18:04:15 +0800

    add n14bitfield,n15 enum.bitfiled is worthy to look.

---
+++ n15-enum/enum1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+enum dice6{
+	ONE = 1,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX
+};
+int main(){
+
+	enum dice6 d6 = TWO;
+
+	d6 = THREE;
+	d6 = 2; //Legal
+	d6 = 9; //Legal	
+
+	int anyint = SIX;
+	printf("now dice = %d\n",d6);
+	printf("now anyint = %d\n",anyint);
+	return 0;
+}
+
+
+/*
+ * 你其实可以把枚举当成单纯的宏定义。有时候没啥差别
+ * 
+ *
+ *
+ */
